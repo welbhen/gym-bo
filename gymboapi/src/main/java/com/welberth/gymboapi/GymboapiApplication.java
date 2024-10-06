ebb509c6d44f7181caec4e67cc11ba4252d3a91e
Welberth Marques <welberth_henrique@hotmail.com>
2024-10-06 14:54:00 -0300

    GYM-3
    Creates GymBoApi Project

---
+++ gymboapi/src/main/java/com/welberth/gymboapi/GymboapiApplication.java
@@ -0,0 +1,13 @@
+package com.welberth.gymboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymboapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymboapiApplication.class, args);
+	}
+
+}
---
+++ gymboapi/src/test/java/com/welberth/gymboapi/GymboapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welberth.gymboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymboapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
