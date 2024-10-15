4ec8e4ad62b0a419ec2066a988a02f9927728ffc
Welberth Marques <welberth_henrique@hotmail.com>
2024-10-14 22:22:22 -0300

    GYM-11
    Creates User table model with its columns and db validations

---
+++ gymboapi/src/main/java/com/welberth/gymboapi/models/User.java
@@ -0,0 +1,119 @@
+package com.welberth.gymboapi.models;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser {
+    }
+
+    ;
+
+    public interface UpdateUser {
+    }
+
+    ;
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+
+    @Column(name = "password", length = 50, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 6, max = 50)
+    private String password;
+
+    @Column(name = "email", length = 100, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 3, max = 100)
+    private String email;
+
+    // TODO:
+    // private Plan activePlan = new Plan();
+    // private Plan previousPlan = new Plan();
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+
+        if (obj == null) return false;
+
+        if (!(obj instanceof User)) return false;
+
+        User other = (User) obj;
+
+        if (this.id == null) {
+            if (other.id != null) return false;
+        }
+
+        return Objects.equals(this.id, other.id) &&
+                Objects.equals(this.username, other.username) &&
+                Objects.equals(this.password, other.password) &&
+                Objects.equals(this.email, other.email);
+    }
+}
