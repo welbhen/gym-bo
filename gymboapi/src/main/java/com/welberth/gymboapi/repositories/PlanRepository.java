27e18e8c413deeaaa480884e52a727a3bb7203bb
Welberth Marques <welberth_henrique@hotmail.com>
2024-10-21 19:33:27 -0300

    GYM-15
    Creates documentation for the Data Access layer for the Plan (PlanRepository)

--- gymboapi/src/main/java/com/welberth/gymboapi/repositories/PlanRepository.java
+++ gymboapi/src/main/java/com/welberth/gymboapi/repositories/PlanRepository.java
@@ -5,6 +5,9 @@ import com.welberth.gymboapi.models.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Data Access layer for the Plan model.
+ */
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long>{
 }
