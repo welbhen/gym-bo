e75e06603380e806d8fedfe4fb13b0fb74c4e1dd
Welberth Marques <welberth_henrique@hotmail.com>
2024-10-23 19:25:41 -0300

    GYM-15
    Updates UserRepository with two new data access methods

--- gymboapi/src/main/java/com/welberth/gymboapi/repositories/UserRepository.java
+++ gymboapi/src/main/java/com/welberth/gymboapi/repositories/UserRepository.java
@@ -5,10 +5,17 @@ import com.welberth.gymboapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Data Access layer for the User model.
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // All methods we need are already available by inheritance of JpaRepository
+    // All main methods we need are already available by inheritance of JpaRepository
+
+    List<User> findByPlan_Id(Long id);
+
+    Optional<User> findByUsername(String username);
 }
